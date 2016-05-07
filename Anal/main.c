32c8717c520795dbbcd9a02e0a4fd7c81f45ba79
John <joaosisanoski@live.com>
2016-05-07 17:52:05 -0300

    tabela hash feita

--- Anal/main.c
+++ Anal/main.c
@@ -12,7 +12,21 @@
 #include "tabsimbolo.h"
 
 int main(int argc, const char * argv[]) {
+/*
+    para fazer o analisador léxico, lembre-se de:
+    alocar uma variável do tipo TNoIdentificador.
+    Colocar os atributos respectivos .
+     char identificador[MAX_ID]; - > variavel que representa o string que identificou o atomo.
+     TCatAtributo tipo_atributo -> olhar enum no arquivo tabsimbolo.h, basicamente representa os tipos de atomos.
+     union conjunto_atributos -> valor utilizado para representar  os tipos da variavel.
 
+
+
+     TOMAR MUITO CUIDADO COM OS PONTEIROS.
+
+
+
+*/
     printf("Hello, World!\n");
     return 0;
 }
